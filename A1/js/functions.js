e4407897e7f3e52ed3df47376110ff3666f98589
uditmalik <umalik1@asu.edu>
2015-09-13 20:11:57 -0700

    Update functions.js

--- A1/js/functions.js
+++ A1/js/functions.js
@@ -1,1 +1,9 @@
 // add all js functions to this file
+
+function validateInput() {
+    		var x = document.getElementById("search").value;      
+    		if (isNaN(x)) 
+			alert("Input must be a number");
+    		else if (x<=0 || x==null) 
+			alert("Input must be greater than zero or not null");
+	}
